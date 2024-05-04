9618dd5f9913c49a714d6e5588f727afbc295e61
github-raul-mello <raul.mello@swile.com.br>
2024-05-04 11:36:05 -0300

    feat: add timeout to hello messages

--- src/main/java/br/usp/raulmello/outbound/Outbox.java
+++ src/main/java/br/usp/raulmello/outbound/Outbox.java
@@ -4,17 +4,19 @@ import br.usp.raulmello.utils.Address;
 import br.usp.raulmello.utils.Message;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class Outbox {
 
     private Outbox() {}
 
     public static boolean sendMessage(final Message message, final Address destination) {
-        try {
-            System.out.println("Creating client socket for destination: " + destination);
-            final Socket clientSocket = new Socket(destination.getDomain(), destination.getPort());
-            System.out.println("Client socket created for destination: " + destination);
+        try (final Socket clientSocket = new Socket()) {
+            System.out.println("Connecting to " + destination);
+            clientSocket.connect(new InetSocketAddress(destination.getDomain(), destination.getPort()), 1000);
+            System.out.println("Connection established with destination: " + destination);
 
             final ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
             out.flush();
@@ -27,14 +29,21 @@ public class Outbox {
             System.out.println("Got response: " + response + " from " + destination);
 
             final boolean success = response.equalsIgnoreCase(message.getOperation() + "_OK");
-
             if (success) {
                 System.out.println("Envio feito com sucesso: \"" + message + "\"");
             }
 
             return success;
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+
+        } catch (final SocketTimeoutException e) {
+            System.err.println("Connection timed out when connecting to " + destination);
+            return false;
+        } catch (final ClassNotFoundException e) {
+            System.err.println("Could not parse response. Reason: " + e.getMessage());
+            return false;
+        } catch (final IOException e) {
+            System.err.println("IO Exception when creating client socket: " + e.getMessage());
+            return false;
         }
     }
 }
