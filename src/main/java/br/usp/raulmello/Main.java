815b1ad5aa4e01c5577205339a176470a885936a
github-raul-mello <raul.mello@swile.com.br>
2024-06-06 11:07:07 -0300

    fix: handle empty values from args

--- src/main/java/br/usp/raulmello/Main.java
+++ src/main/java/br/usp/raulmello/Main.java
@@ -3,6 +3,7 @@ package br.usp.raulmello;
 import br.usp.raulmello.ui.ParametersFileReader;
 import br.usp.raulmello.ui.Logger;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -10,7 +11,7 @@ public class Main {
     public static void main(final String[] args) {
         final String hostAddress = args[0];
         final int hostPort = Integer.parseInt(args[1]);
-        final Map<String, String> values = ParametersFileReader.readDataFromValuesFile(args[3]);
+        final Map<String, String> values = getValuesFromArgs(args);
         final List<String> neighborsList = ParametersFileReader.readNeighboursFromFile(args[2]);
 
         Logger.debug("Initializing node with host: {} port: {} neighbors: {} values: {}"
@@ -18,5 +19,13 @@ public class Main {
         final Node node = Node.initNode(hostAddress, hostPort, values, neighborsList);
         Logger.debug("Starting node");
         node.startNode();
+    }
+
+    private static Map<String, String> getValuesFromArgs(final String[] args) {
+        if (args.length > 3) {
+            return ParametersFileReader.readDataFromValuesFile(args[3]);
+        }
+
+        return Collections.emptyMap();
     }
 }
