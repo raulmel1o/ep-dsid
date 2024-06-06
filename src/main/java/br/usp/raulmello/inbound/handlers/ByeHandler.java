501986724ff166b05706f81745cb4564ddbce611
github-raul-mello <raul.mello@swile.com.br>
2024-06-06 20:15:40 -0300

    feat: response BYE_OK

--- src/main/java/br/usp/raulmello/inbound/handlers/ByeHandler.java
+++ src/main/java/br/usp/raulmello/inbound/handlers/ByeHandler.java
@@ -4,6 +4,9 @@ import br.usp.raulmello.Node;
 import br.usp.raulmello.ui.Logger;
 import br.usp.raulmello.utils.Message;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class ByeHandler extends AbstractHandler {
     public ByeHandler(final Node nodeContext, final Message message) {
         super(null, nodeContext, message);
@@ -11,6 +14,14 @@ public class ByeHandler extends AbstractHandler {
 
     @Override
     public void run() {
+        try {
+            final ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+            out.flush();
+            out.writeObject("BYE_OK");
+        } catch (IOException e) {
+            Logger.debug("Got exception: {}", e.getMessage());
+        }
+
         Logger.info("Mensagem recebida: \"{}\"", message);
         nodeContext.getNeighbors().remove(message.getOrigin());
         Logger.info("Removendo vizinho da tabela {}", message.getOrigin());
