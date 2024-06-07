97a55b1809222f1b8b38070e7c49fe5343d22fb8
github-raul-mello <raul.mello@swile.com.br>
2024-06-06 22:31:56 -0300

    fix: return VAL_OK to origin

--- src/main/java/br/usp/raulmello/inbound/handlers/ValHandler.java
+++ src/main/java/br/usp/raulmello/inbound/handlers/ValHandler.java
@@ -4,6 +4,8 @@ import br.usp.raulmello.Node;
 import br.usp.raulmello.ui.Logger;
 import br.usp.raulmello.utils.Message;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class ValHandler extends AbstractHandler {
@@ -13,6 +15,15 @@ public class ValHandler extends AbstractHandler {
 
     @Override
     public void run() {
+        try {
+            final ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+            out.flush();
+            out.writeObject("VAL_OK");
+
+        } catch (IOException e) {
+            Logger.debug("Got exception: {}", e.getMessage());
+        }
+
         Logger.info("Mensagem recebida: \"{}\"", message.toString().replace("\n", ""));
         Logger.info("\tValor encontrado!");
         Logger.info("\t\tchave: {} valor: {}", message.getArgs().get(1), message.getArgs().get(2));
