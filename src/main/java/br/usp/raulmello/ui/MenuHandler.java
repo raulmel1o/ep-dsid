4bb66b0bc377d560d3813219e67b995bcc47bccd
github-raul-mello <raul.mello@swile.com.br>
2024-06-09 19:04:50 -0300

    feat: configure DFS context when stating a DFS

--- src/main/java/br/usp/raulmello/ui/MenuHandler.java
+++ src/main/java/br/usp/raulmello/ui/MenuHandler.java
@@ -1,11 +1,11 @@
 package br.usp.raulmello.ui;
 
+import br.usp.raulmello.DfsContext;
 import br.usp.raulmello.Node;
 import br.usp.raulmello.outbound.Outbox;
 import br.usp.raulmello.utils.Address;
 import br.usp.raulmello.utils.Message;
 
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -87,14 +87,18 @@ public class MenuHandler {
         if (nodeContext.getValues().containsKey(key)) {
             showKeyIsInLocalStorage(key, nodeContext.getValues().get(key));
         } else {
+            // define DFS context
+            final Address nextNode = nodeContext.getNeighbors().get(random.nextInt(nodeContext.getNeighbors().size()));
+
+            final DfsContext dfsContext = nodeContext.getDfsContext();
+            dfsContext.setMotherNode(nodeContext.getHostAddress());
+            dfsContext.setNextNode(nextNode);
+            dfsContext.setActiveNeighbor(nextNode);
+            dfsContext.setEligibleNeighbors(nodeContext.getNeighbors().stream().filter(n -> n != nextNode).toList());
+
             final Message message = createSearchDepthFirstMessage(key, nodeContext.getHostAddress(), nodeContext.getSequenceNumber(), nodeContext.getTtl());
 
-            final List<Address> availableNeighbors = nodeContext.getNeighbors();
-            final Address selectedNeighbor = nodeContext.getNeighbors().get(random.nextInt(nodeContext.getNeighbors().size()));
-            final Address activeNeighbor = selectedNeighbor;
-            availableNeighbors.remove(selectedNeighbor);
-
-            Outbox.sendMessage(message, selectedNeighbor);
+            Outbox.sendMessage(message, nextNode);
             nodeContext.setSequenceNumber(nodeContext.getSequenceNumber() + 1);
             nodeContext.getNodeStats().incrementSentDepthFirstSearchMessageAmount();
         }
